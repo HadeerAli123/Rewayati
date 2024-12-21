e33157b42e3618d845eb9367c0d84615d104eddd
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-12-21 19:42:33 +0200

    Rechange file direction

---
+++ .angular/cache/18.2.11/front-end/vite/deps/@angular_material_icon.js
@@ -0,0 +1,37 @@
+import {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+} from "./chunk-MZYQTKQP.js";
+import "./chunk-QMMIJY6W.js";
+import "./chunk-KSTT4RLP.js";
+import "./chunk-ERB2HXUP.js";
+import "./chunk-3PDWXPZX.js";
+import "./chunk-2TY7BGOM.js";
+import "./chunk-QXNCJGT5.js";
+import "./chunk-ZSY7TSMJ.js";
+import "./chunk-4MWRP73S.js";
+export {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+};
+//# sourceMappingURL=@angular_material_icon.js.map
