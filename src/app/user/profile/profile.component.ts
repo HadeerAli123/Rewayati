ad6bd1a942a7b0914fa181f79263dc718b44f7bb
esraaKamel1195 <esraa.kamel1811@gmail.com>
2025-01-05 12:17:03 +0200

    Profile Enhancements

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,18 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from '../../services/users.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+  ],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent implements OnInit {
-
   userSubscription?: Subscription;
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
 
   constructor(private usersServices: UsersService) {}
 
@@ -20,10 +37,20 @@ export class ProfileComponent implements OnInit {
     this.usersServices.getCurrentUser().subscribe({
       next: (response) => {
         console.log(response);
-      }, error: (error) => {
+      },
+      error: (error) => {
         console.log(error);
-      }
+      },
     });
   }
 
+  save() {
+    console.log('User information saved:', this.user);
+    alert('Profile saved successfully!');
+  }
+
+  cancel() {
+    console.log('Action canceled');
+    alert('Changes canceled!');
+  }
 }
