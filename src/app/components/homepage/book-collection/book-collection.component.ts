9a583fc565be85942dac02892282c3c814510872
esraaKamel1195 <esraa.kamel1811@gmail.com>
2025-01-05 12:18:29 +0200

    Popular stories

--- src/app/components/homepage/book-collection/book-collection.component.ts
+++ src/app/components/homepage/book-collection/book-collection.component.ts
@@ -1,45 +1,53 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { StoryService } from '../../../services/story.service';
+import { Story } from '../../../interfaces/story';
 
 @Component({
   selector: 'app-book-collection',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './book-collection.component.html',
-  styleUrl: './book-collection.component.css'
+  styleUrl: './book-collection.component.css',
 })
-export class BookCollectionComponent {
-  
-  books: any[] =[
-    {title: 'Book 1', author: 'Author 1', genre: 'Fiction'},
-    {title: 'Book 2', author: 'Author 2', genre: 'Non-Fiction'},
-    {title: 'Book 3', author: 'Author 3', genre: 'Science Fiction'},
-    {title: 'Book 4', author: 'Author 4', genre: 'Thriller'},
-    {title: 'Book 5', author: 'Author 5', genre: 'Mystery'},
-    {title: 'Book 6', author: 'Author 6', genre: 'Romance'},
-    {title: 'Book 7', author: 'Author 7', genre: 'Adventure'},
-    {title: 'Book 9', author: 'Author 8', genre: 'Comedy'},
-    {title: 'Book 10', author: 'Author 8', genre: 'Comedy'},
-    {title: 'Book 11', author: 'Author 8', genre: 'Comedy'},
-    {title: 'Book 12', author: 'Author 8', genre: 'Comedy'},
-    {title: 'Book 13', author: 'Author 8', genre: 'Comedy'},
-
-  ]
-
+export class BookCollectionComponent implements OnInit {
   visibleBook: any[] = [];
-  booksPerPage: number[] =[];
+  booksPerPage: number[] = [];
+  stories: Array<Story> = [];
 
-  constructor() {
-    this.loadInit()
-   }
-loadInit(){
-  this.visibleBook = this.books.slice(0, 6);    
-}
-
-  loadPrevious(){
-    const startIndex = this.visibleBook.length - 6;
-    this.visibleBook = this.books.slice(startIndex, startIndex + 6);
+  constructor(private router: Router, private storiesServices: StoryService) {
+    this.loadInit();
   }
-  loadMore(){}
+
+  ngOnInit(): void {
+    this.storiesServices.getTopStoriesByViews().subscribe({
+      next: (res) => {
+        console.log('res', res);
+        this.stories = res;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  loadInit() {
+    // this.visibleBook = this.books.slice(0, 6);
+  }
+
+  loadPrevious() {
+    // const startIndex = this.visibleBook.length - 6;
+    // this.visibleBook = this.books.slice(startIndex, startIndex + 6);
+  }
+
+  showDetails(storyId: number | string) {
+    this.router.navigateByUrl(``);
+  }
+
+  loadMore() {
+    this.router.navigateByUrl(``);
+  }
 }
