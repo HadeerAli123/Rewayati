4096a4fbe54c7b52c6c9d2f5c1ca4bbe9ecb8b5f
esraaKamel1195 <esraa.kamel1811@gmail.com>
2025-01-05 12:22:45 +0200

    categories carousel

--- src/app/components/homepage/category-carousel/category-carousel.component.ts
+++ src/app/components/homepage/category-carousel/category-carousel.component.ts
@@ -48,6 +48,6 @@ export class CategoryCarouselComponent {
 
   goToCategoryPage(cat_id: string) {
     console.log(cat_id);
-    this.route.navigateByUrl(`stories/${cat_id}`);
+    this.route.navigateByUrl(`stories-by-category/${cat_id}`);
   }
 }
