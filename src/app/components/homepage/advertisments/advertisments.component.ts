4a4bc422c3631ffc3d74b3f31d699cca39e72c59
esraaKamel1195 <esraa.kamel1811@gmail.com>
2025-01-05 12:19:31 +0200

    Advertisments latest published stories

--- src/app/components/homepage/advertisments/advertisments.component.ts
+++ src/app/components/homepage/advertisments/advertisments.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { StoryService } from '../../../services/story.service';
+import { Story } from '../../../interfaces/story';
 
 @Component({
   selector: 'app-advertisments',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule, CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './advertisments.component.html',
   styleUrl: './advertisments.component.css'
 })
-export class AdvertismentsComponent {
+export class AdvertismentsComponent implements OnInit {
+  stories: Array<Story> = [];
+
+  constructor(private storyServices: StoryService) {}
+
+  ngOnInit(): void {
+    this.storyServices.getAdvertisementStoryByLatestStory().subscribe({
+      next: (res) => {
+        console.log('latest', res);
+        this.stories = res;
+      }, error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 
 }
